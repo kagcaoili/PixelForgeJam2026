c86e85dbc32d937dd4b446265d3d4f7b11ca79f4
kagcaoili <kagcaoil@gmail.com>
2026-04-18 16:10:17 -0700

    Added initial scaffolding for game system scripts, input, and prototype scenes to mess around in

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public int Score { get; private set; }
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    /// <summary>
+    /// Called from other scripts to add score the player's total score
+    /// </summary>
+    /// <param name="amount"></param>
+    public void AddScore(int amount)
+    {
+        Score += amount;
+    }
+}
