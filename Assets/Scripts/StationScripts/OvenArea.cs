6158b5c69216ab51a66e6617eac20d9730fe98a3
kagcaoili <kagcaoil@gmail.com>
2026-04-23 23:28:05 -0700

    Fixed seared salmon to be seared tuna. also changed stove to oven. made oven

---
+++ Assets/Scripts/StationScripts/OvenArea.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+/// <summary>
+/// Player interact with pot to convert item to something else, no need to hold
+/// </summary>
+public class OvenArea : Station
+{
+    private void Update()
+    {
+        if (currentRule == null || currentRule.requiresHold) return;
+
+        progress += Time.deltaTime;
+        UpdateProgressBar();
+
+        if (progress >= currentRule.duration)
+        {
+            Transform(); // turn input item into output item
+        }
+    }
+}
